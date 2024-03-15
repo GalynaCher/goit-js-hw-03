5eefabe02c739ebbd28b18ead0a6e6b0c4898256
Galyna Ch <kirsikka.gala@gmail.com>
2024-03-15 15:52:21 +0200

    HW#3: Version #1

---
+++ js/task-3.js
@@ -0,0 +1,22 @@
+/* Task#3: Filter Array */
+
+'use strict';
+
+function filterArray(numbers, value) { 
+
+    let resultArray = [];
+
+    for (let index = 0; index < numbers.length; index++) {
+        if (numbers[index] > value) {
+            resultArray.push(numbers[index]);
+        }
+    }
+
+    return resultArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
